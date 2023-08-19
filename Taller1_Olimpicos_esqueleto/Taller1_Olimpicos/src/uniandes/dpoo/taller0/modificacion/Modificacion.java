5038a01ca447ff22dd99039705bca5e40fc253f1
juan-sebastian-castro-garay <78106495+juan-sebastian-castro-garay@users.noreply.github.com>
2023-08-19 00:49:53 -0500

    Add files via upload

---
+++ Taller1_Olimpicos_esqueleto/Taller1_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,29 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.consola.ConsolaOlimpicos;
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+/**
+ * Clase modificacion.
+ */
+public class Modificacion {
+	/**
+	 * Este es el método principal de la aplicación, con el que inicia la ejecución
+	 * de la aplicación
+	 * 
+	 * @param args Parámetros introducidos en la línea de comandos al invocar la
+	 *             aplicación
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+	}
+}
